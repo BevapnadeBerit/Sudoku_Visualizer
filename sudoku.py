21882f9201ba59fd058a2dc25ed6513de4f84315
Alec Pramanik <alec.pramanik@gmail.com>
2023-04-29 20:47:09 +0200

    Add Sudoku Class Skeleton

---
+++ sudoku.py
@@ -0,0 +1,15 @@
+from grid import Grid, Square, Box
+
+class Sudoku:
+    def __init__(self, grid: Grid):
+        pass
+
+    def insert_number(self, row: int, col: int, num: int) -> bool:
+        pass
+
+    def is_number_valid(self, row: int, col: int, num: int) -> bool:
+        pass
+
+# Add the puzzle creation class later
+# class PuzzleGenerator:
+#     pass
