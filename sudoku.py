e8f024fead451c181d07fd008f6bfaf2542b13bf
Alec Pramanik <alec.pramanik@gmail.com>
2023-04-30 21:25:59 +0200

    Optimize Sudoku class to make use of new Square row/col fields

--- sudoku.py
+++ sudoku.py
@@ -14,49 +14,28 @@ class Sudoku:
         group = square.groups()[0]
         square.set_value(value, group)
 
-    def is_valid(self, square: Square, value: int) -> bool:
-        box = self._get_box(square)
-        if self._value_in_box(box, value):
-            return False
-
-        if self._value_in_row(square, value):
-            return False
-
-        if self._value_in_column(square, value):
-            return False
-
-        return True
+    def is_number_valid(self, row: int, col: int, num: int) -> bool:
+        # Check if the number is already in the same row or column
+        for i in range(9):
+            if self.grid.squares[row][i].value == num:
+                return False
+            if self.grid.squares[i][col].value == num:
+                return False
+        
+        # Check if the number is already in the same box
+        box = self._get_box(self.grid.squares[row][col])
+        return not self._value_in_box(box, num)
 
     def _get_box(self, square: Square) -> Box:
-        box_x = (square.pos[0] - self.grid_pos[0] + BOX_SIZE // 2) // BOX_SIZE
-        box_y = (square.pos[1] - self.grid_pos[1] + BOX_SIZE // 2) // BOX_SIZE
-        return self.boxes[box_x][box_y]
+        box_x = square.col // 3
+        box_y = square.row // 3
+        return self.grid.boxes[box_y][box_x]
     
     def _value_in_box(self, box: Box, value: int) -> bool:
         for square_row in box.squares:
             for square in square_row:
                 if square.value == value:
                     return True
-        return False
-    
-    def _value_in_row(self, square: Square, value: int) -> bool:
-        for box_row in self.grid.boxes:
-            for box in box_row:
-                for square_row in box.squares:
-                    if square in square_row:
-                        for sq in square_row:
-                            if sq.value == value:
-                                return True
-        return False
-
-    def _value_in_column(self, square: Square, value: int) -> bool:
-        for box_col in self.grid.boxes:
-            for box in box_col:
-                for square_col_idx in range(len(box.squares)):
-                    if square in box.squares[square_col_idx]:
-                        for square_col in box_col:
-                            if square_col.squares[square_col_idx].value == value:
-                                return True
         return False
 
 # Add the puzzle creation class later
