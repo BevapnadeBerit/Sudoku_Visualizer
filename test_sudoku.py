a5a772b3e7b42807509c2b29c67688c4a023b87e
Alec Pramanik <alec.pramanik@gmail.com>
2023-05-05 01:34:36 +0200

    Update test_sudoku to reflect changes

--- test_sudoku.py
+++ test_sudoku.py
@@ -17,32 +17,60 @@ class TestSudoku(unittest.TestCase):
         self.grid = Grid(grid_pos, 3, 3, 3, 3)
         self.sudoku = Sudoku(self.grid, grid_pos)
 
-    def test_insert_number(self):
+    def test_insert_number_full_grid(self):
+        # Insert random values in all squares
         for row in range(9):
             for col in range(9):
-                for num in range(1, 10):
-                    self.sudoku.remove_number(row, col)  # Clear the square before each insertion attempt
-                    if self.sudoku.is_number_valid(row, col, num):
-                        self.assertTrue(self.sudoku.insert_number(row, col, num))
-                        self.assertEqual(self.sudoku.get_number(row, col), num)
-                    else:
-                        self.assertFalse(self.sudoku.insert_number(row, col, num))
+                value = random.randint(1, 9)
+                self.sudoku.insert_number(row, col, value)
+                square = self.sudoku._get_square(row, col)
+                #print(row, col, square.value, value)
+                #print(square.valid)
+                is_valid = self.sudoku.is_number_valid(row, col, value)
+                #print(is_valid)
+                if not is_valid:
+                    self.assertFalse(square.valid)
+                else:
+                    self.assertTrue(square.valid)
 
-    def test_insert_invalid_numbers(self):
+    def test_insert_out_of_range_numbers(self):
         for num in range(-10, 20):
             if num not in range(1, 10):
                 for row in range(9):
                     for col in range(9):
                         self.assertFalse(self.sudoku.insert_number(row, col, num))
 
-    def test_insert_valid_numbers(self):
-        for num in range(1, 10):
-            for _ in range(10):  # Test 10 random positions for each number
+    def test_insert_number_valid(self):
+        for _ in range(10):  # Test 10 random positions for each number
+            for num in range(1, 10):
                 row, col = random.randint(0, 8), random.randint(0, 8)
                 is_valid = self.sudoku.is_number_valid(row, col, num)
-                is_inserted = self.sudoku.insert_number(row, col, num)
-                self.assertEqual(is_inserted, is_valid)
+                if is_valid:
+                    self.assertTrue(self.sudoku.insert_number(row, col, num))
+                    self.assertEqual(self.sudoku.get_number(row, col), num)
+                    square = self.sudoku._get_square(row, col)
+                    self.assertTrue(square.valid)
                 # Clean up the inserted number for the next iteration
+                self.sudoku.remove_number(row, col)
+
+    def test_insert_number_invalid(self):
+        for _ in range(10):  # Test 10 random positions for each number
+            for num in range(1, 10):
+                row, col = random.randint(0, 8), random.randint(0, 8)
+                # Insert the number in the same row, column, and box to make it invalid
+                self.sudoku.insert_number(row, (col + 1) % 9, num)
+                self.sudoku.insert_number((row + 1) % 9, col, num)
+                self.sudoku.insert_number((row + 1) % 9, (col + 1) % 9, num)
+                is_valid = self.sudoku.is_number_valid(row, col, num)
+                if not is_valid:
+                    self.assertTrue(self.sudoku.insert_number(row, col, num))
+                    self.assertEqual(self.sudoku.get_number(row, col), num)
+                    square = self.sudoku._get_square(row, col)
+                    self.assertFalse(square.valid)
+                # Clean up the inserted numbers for the next iteration
+                self.sudoku.remove_number(row, (col + 1) % 9)
+                self.sudoku.remove_number((row + 1) % 9, col)
+                self.sudoku.remove_number((row + 1) % 9, (col + 1) % 9)
                 self.sudoku.remove_number(row, col)
 
     def test_remove_number(self):
