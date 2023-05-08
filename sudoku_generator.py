0f3543ab44314599e60a9248c8c77684bc2d4a78
Alec Pramanik <alec.pramanik@gmail.com>
2023-05-08 20:34:45 +0200

    Add SudokuGenerator skeleton

--- sudoku_generator.py
+++ sudoku_generator.py
@@ -1,3 +1,33 @@
 import copy
 import random
 
+class SudokuGenerator:
+
+    def __init__(self):
+        self.grid = [[0 for _ in range(9)] for _ in range(9)]
+
+    def generate_puzzle(self):
+        # Generate a completely filled Sudoku grid
+        # Remove some cells to create the puzzle
+        pass
+        
+    def generate_solution(self, grid):
+        # Generate a completely filled Sudoku grid
+        pass
+        
+    def is_valid(self, grid, row, col, num):
+        # Check if placing a number in a cell is valid
+        pass
+
+    def remove_cells(self, grid, num_cells):
+        # Remove given number of cells from the grid to create the puzzle
+        pass
+        
+    def print_grid(self, grid):
+        # Print the Sudoku grid in a readable format
+        pass
+
+if __name__ == '__main__':
+    generator = SudokuGenerator()
+    generator.generate_puzzle()
+    generator.print_grid(generator.grid)
